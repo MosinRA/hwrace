dc0ad28cce1b196b6c354ebc2f5c5dbdb78b20c0
Roman Mosin <mosinra90@yandex.ru>
2020-07-01 10:18:05 +0500

    HW Race

---
+++ src/Tunel.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.Semaphore;
+
+class Tunnel extends Stage {
+    Semaphore smp = new Semaphore(Main.HALF_CARS_COUNT);
+
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " ождает к этап: " + description);
+                smp.acquire();
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+                smp.release();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
